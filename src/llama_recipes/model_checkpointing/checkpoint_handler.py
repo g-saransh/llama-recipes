d9cb2cebc027e0993fe5d3d6b39223a9411a9cbd
Saransh Gupta <saransh@ibm.com>
2024-03-12 17:27:42 -0700

    additional logging

--- src/llama_recipes/model_checkpointing/checkpoint_handler.py
+++ src/llama_recipes/model_checkpointing/checkpoint_handler.py
@@ -141,6 +141,7 @@ def save_model_and_optimizer_sharded(model, rank, cfg,optim=None):
         state_dict = {"model": model.state_dict()}
         t_state = time.perf_counter()
         print(f"Checkpoint state_dict creation time = {t_state-t0:.4f}")
+        
         if optim is not None:
             state_dict["optim"] = FSDP.optim_state_dict(model, optim)
             print(f"adding optim to state_dict")
@@ -202,8 +203,10 @@ def save_model_and_optimizer_sharded(model, rank, cfg,optim=None):
                 planner=DefaultSavePlanner(),
                 
             )
+    t_b = time.perf_counter()
     dist.barrier()
     t1 = time.perf_counter()
+    print(f"Checkpoint barrier time = {t1-t_b:.4f}")
     if rank == 0:
         print(f"Sharded state checkpoint saved to {save_dir}")
         print(f"Checkpoint Time = {t1-t0:.4f}")
