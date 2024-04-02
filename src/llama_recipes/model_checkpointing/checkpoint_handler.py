d12f7f3dff70266012eeb9ffc170759ac389a9ed
Saransh Gupta <saransh@ibm.com>
2024-04-02 10:24:07 -0700

    update async_profile

--- src/llama_recipes/model_checkpointing/checkpoint_handler.py
+++ src/llama_recipes/model_checkpointing/checkpoint_handler.py
@@ -199,19 +199,20 @@ def save_model_and_optimizer_sharded(epoch, model, rank, cfg,optim=None):
             print(f"kinesis: Checkpoint memory copy time (rank {rank})... {time.perf_counter() - t_m}")
             
             if (cfg.profile_writeout):
-                executor = ThreadPoolExecutor(max_workers=1)
-                executor.submit(
-                    profile_async_writeout,
-                    f,
-                    rank,
-                    epoch,
-                )
-                if not cfg.profile_writeout_blocking:
-                    print(f"Non-blocking writeout profiling")
-                    executor.shutdown(wait=False)
-                else:
-                    print(f"Blocking writeout profiling")
-                    executor.shutdown(wait=True)
+                with ThreadPoolExecutor(max_workers=1) as executor:
+                # executor = ThreadPoolExecutor(max_workers=1)
+                    executor.submit(
+                        profile_async_writeout,
+                        f,
+                        rank,
+                        epoch,
+                    )
+                    if not cfg.profile_writeout_blocking:
+                        print(f"Non-blocking writeout profiling")
+                        executor.shutdown(wait=False)
+                    else:
+                        print(f"Blocking writeout profiling")
+                        executor.shutdown(wait=True)
 
         else:
             print(f"Doing sync checkpointing to {save_dir}")
